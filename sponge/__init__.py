e001d5691b25928a3dccc49d84293ffddbf0ba09
IamBusy <1342247033@qq.com>
2018-05-22 16:33:49 +0800

    Driver: add memory

---
+++ sponge/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: william
+@contact: 1342247033@qq.com
+@site: http://www.xiaolewei.com
+@file: __init__.py.py
+@time: 22/05/2018 15:59
+"""
+from .manager import CacheManager
+__all__ = [CacheManager]
---
+++ sponge/drivers/memory.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: william
+@contact: 1342247033@qq.com
+@site: http://www.xiaolewei.com
+@file: arraydriver.py
+@time: 22/05/2018 16:15
+"""
+import time
+from sponge.drivers.driver import Driver
+
+
+class MemoryDriver(Driver):
+
+    def __init__(self):
+        self._store = {}
+
+    def get(self, key):
+        if key in self._store and (self._store[key]['ttl'] == 0 or self._store[key]['ttl'] > time.time()):
+            return self._store[key]['val']
+        return None
+
+    def put(self, key, value, secs):
+        self._store[key] = {
+            'val': value,
+            'ttl': time.time() + secs
+        }
+
+    def increase(self, key, value=1):
+        if key in self._store:
+            if self._store[key]['ttl'] == 0 or self._store[key]['ttl'] > time.time():
+                self._store[key]['val'] += value
+
+    def decrease(self, key, value=1):
+        if key in self._store:
+            if self._store[key]['ttl'] == 0 or self._store[key]['ttl'] > time.time():
+                self._store[key]['val'] -= value
+
+    def forget(self, key):
+        if key in self._store:
+            del self._store[key]
+
+    def forever(self, key, value):
+        self._store[key] = {
+            'val': value,
+            'ttl': 0
+        }
+
